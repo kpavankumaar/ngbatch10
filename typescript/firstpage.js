2baaeef269e28b7fa2d476d209b732b6288d1332
nbitsmeanstack49 <nbitsmeanstack49@gmail.com>
2019-05-08 09:37:52 +0530

    typescript let const declaretions

---
+++ typescript/firstpage.js
@@ -0,0 +1,15 @@
+// function sum(a, b) {
+//     console.log(a + b);
+// }
+// sum(10, 'test');
+// run this code in js file 
+// var num1 = 10;
+// let num2 = 20;
+// console.log(num2);
+// for(let i = 0; i < 4; i = i + 1){
+//     setTimeout(function(){
+//         console.log(i);
+//     },1000);
+// }
+var name = 'ravi';
+name = 'srk';
