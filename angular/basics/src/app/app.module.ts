141e978d05d0286bf0eb2907bb0713b04943eeed
nbitsmeanstack49 <nbitsmeanstack49@gmail.com>
2019-05-29 09:02:28 +0530

    routings

--- angular/basics/src/app/app.module.ts
+++ angular/basics/src/app/app.module.ts
@@ -1,23 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { TemplatedrivenformComponent } from './templatedrivenform/templatedrivenform.component';
 import { ReactiveformComponent } from './reactiveform/reactiveform.component';
+import { SamplecomponentComponent } from './reactiveform/samplecomponent/samplecomponent.component';
 
-
+const route:Routes = [
+  {path:'templatedrivenform', component:TemplatedrivenformComponent},
+  { path:'reactiveform', 
+    component:ReactiveformComponent,
+    children:[
+      {path:'sample',component:SamplecomponentComponent}
+    ]
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     TemplatedrivenformComponent,
-    ReactiveformComponent
+    ReactiveformComponent,
+    SamplecomponentComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(route)
   ],
   providers: [],
   bootstrap: [AppComponent]
