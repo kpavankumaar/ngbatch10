2e6f5d7e5951799d03f6a9cf2ba4864d355bfe43
nbitsmeanstack49 <nbitsmeanstack49@gmail.com>
2019-06-07 08:50:05 +0530

    created data.service file , served build files from node platfrom, differentiated ng build , ng build --aot, ngbuild --watch, ng serve

--- angular/customermanager/src/app/core/core.module.ts
+++ angular/customermanager/src/app/core/core.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
-
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    HttpClientModule
   ],
   declarations: [NavbarComponent],
   exports:[NavbarComponent]
